caa3714260b6ce534c83cb88b5f605baa338a403
FirozAli1 <61276097+FirozAli1@users.noreply.github.com>
2022-12-04 05:30:39 +0100

    Add files via upload

---
+++ constant.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  9 18:40:11 2021
+
+@author: ASUS
+""
+
+fro
+
+# printing the liter constant from scipy library
+
+# from scipy import constants
+
+
+# print(constants.liter)
+
+
+
+
+# using __version__ attributes to find the version of the scipy library
+
+# import scipy
+
+# print(scipy.__version__)
+
+
+
+
+# a list of all units under the constants module can be seen using the dir() function
+from scipy import constants
+
+print(dir(constants))
+
+
+
+# now started to print all the constant in scitific field
+
+# The "Avogadro" constants
+print(constants.Avogadro)
+
+
+
+
+
+# "Metric" system
+
+print()
+print("----------------------------------------")
+
+print(constants.yotta)
+print("-----------------------------------------")
+print("zetta : ", constants.zetta)
+print("--------------------------------------")
+print("exa : ", constants.exa)
+print("--------------------------------------")
+print("peta : ", constants.peta)
+print("------------------------------------")
+print("tera : ", constants.tera)
+print("--------------------------------------")
+print("giga : ", constants.giga)
+print("--------------------------------------")
+print("mega : ", constants.mega)
+print("-----------------------------------------")
+print("kilo : ", constants.kilo)
+print("-------------------------------------")
+print("hecto : ", constants.hecto)
+print("-----------------------------------")
+print("deka : ", constants.deka)
+print("------------------------------------")
+print("deci : ", constants.deci)
+
+
+
+print('Now the small part')
+
+print("-----------------------------------------")
+print("--------------------------------------")
+print("centi : ", constants.centi)
+print("----------------------------------")
+print("milli : ", constants.milli)
+print("----------------------------------")
+print("nano : ", constants.nano)
+print("-------------------------------------")
+print("pico : ", constants.pico)
+print("---------------------------------------")
+print("femto : ", constants.femto)
+print("------------------------------------")
+print("atto : ", constants.atto)
+print("------------------------------------")
+print("zepto : ", constants.zepto)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
